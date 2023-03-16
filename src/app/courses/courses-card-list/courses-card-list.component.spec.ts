819299f9b06ecf2814e7fffa5a868ac96501acd2
Morgan <morgan.espindola@gmail.com>
2023-03-16 20:37:10 -0300

    Section 03: Introduction to Angular Component Testing - Presentational Components

---
+++ src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -0,0 +1,22 @@
+import { TestBed } from "@angular/core/testing";
+import { CoursesModule } from "../courses.module";
+
+describe("CoursesCardListComponent", () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [CoursesModule],
+    });
+  });
+
+  it("should create the component", () => {
+    pending();
+  });
+
+  it("should display the course list", () => {
+    pending();
+  });
+
+  it("should display the first course", () => {
+    pending();
+  });
+});
