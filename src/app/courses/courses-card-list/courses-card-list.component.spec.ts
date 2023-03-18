f416e4f4d8372b8539c254c91348253e4def6218
Morgan <morgan.espindola@gmail.com>
2023-03-18 12:01:22 -0300

    Section 03: Angular Test Debugging - How to Trigger Change Detection?

--- src/app/courses/courses-card-list/courses-card-list.component.spec.ts
+++ src/app/courses/courses-card-list/courses-card-list.component.spec.ts
@@ -30,14 +30,28 @@ describe("CoursesCardListComponent", () => {
   it("should display the course list", () => {
     component.courses = setupCourses();
     fixture.detectChanges();
-
     const cards = element.queryAll(By.css(".course-card"));
 
-    expect(cards).toBeTruthy("Could not find cards")
+    expect(cards).toBeTruthy("Could not find cards");
     expect(cards.length).toBe(12, "Unexpected number of courses");
   });
 
   it("should display the first course", () => {
-    pending();
+    component.courses = setupCourses();
+    fixture.detectChanges();
+    const firstCourse = component.courses[0];
+    const card = element.query(By.css(".course-card:first-child"));
+    const title = card.query(By.css("mat-card-title"));
+    const image = card.query(By.css("img"));
+
+    expect(card).toBeTruthy("Could not find the first course card");
+    expect(title.nativeElement.textContent).toBe(
+      firstCourse.titles.description,
+      "Title on template does not match with the data"
+    );
+    expect(image.nativeElement.src).toBe(
+      firstCourse.iconUrl,
+      "Image source on template does not match with the data"
+    );
   });
 });
