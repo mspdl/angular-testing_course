64fbc7b3a48aa43f3a93377ddcfaabe77120463d
Morgan <morgan.espindola@gmail.com>
2023-03-18 12:28:54 -0300

    Section 03: Testing Angular Smart or Container Components - Test Setup

---
+++ src/app/courses/home/home.component.spec.ts
@@ -0,0 +1,49 @@
+import { DebugElement } from "@angular/core";
+import { ComponentFixture, TestBed, waitForAsync } from "@angular/core/testing";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { CoursesModule } from "../courses.module";
+import { CoursesService } from "../services/courses.service";
+import { HomeComponent } from "./home.component";
+
+describe("HomeComponent", () => {
+  let fixture: ComponentFixture<HomeComponent>;
+  let component: HomeComponent;
+  let debugElement: DebugElement;
+
+  beforeEach(waitForAsync(() => {
+    const coursesServiceSpy = jasmine.createSpyObj("CoursesService", [
+      "findAllCourses",
+    ]);
+
+    TestBed.configureTestingModule({
+      imports: [CoursesModule, NoopAnimationsModule],
+      providers: [{ provide: CoursesService, useValue: coursesServiceSpy }],
+    })
+      .compileComponents()
+      .then(() => {
+        fixture = TestBed.createComponent(HomeComponent);
+        component = fixture.componentInstance;
+        debugElement = fixture.debugElement;
+      });
+  }));
+
+  it("should create the component", () => {
+    expect(component).toBeTruthy();
+  });
+
+  it("should display only beginner courses", () => {
+    pending();
+  });
+
+  it("should display only advanced courses", () => {
+    pending();
+  });
+
+  it("should display both tabs", () => {
+    pending();
+  });
+
+  it("should display advanced courses when tab clicked", () => {
+    pending();
+  });
+});
