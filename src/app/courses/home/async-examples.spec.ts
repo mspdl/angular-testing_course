6331ebe1cefd5b3378c9e7d128f16f0ffb8b1b99
Morgan <morgan.espindola@gmail.com>
2023-03-24 22:37:28 -0300

    Section 04: Understanding the Angular fakeAsync Testing Zone

--- src/app/courses/home/async-examples.spec.ts
+++ src/app/courses/home/async-examples.spec.ts
@@ -1,11 +1,28 @@
-fdescribe("Async Testing Examples", () => {
+import { fakeAsync, flush } from "@angular/core/testing";
+
+describe("Async Testing Examples", () => {
   it("Asynchronous test example with Jasmine done()", (done: DoneFn) => {
     let test = false;
     setTimeout(() => {
-      console.log("running asserions");
+      console.log("running assertions");
       test = true;
-      expect(test).toBeTruthy;
+      expect(test).toBeTruthy();
       done();
     }, 1000);
   });
+
+  it("Asynchronous test example - setTimeout()", fakeAsync(() => {
+    let test = false;
+
+    setTimeout(() => {});
+
+    setTimeout(() => {
+      console.log("running assertions setTimeout()");
+      test = true;
+    }, 1000);
+
+    expect(test).toBeFalse();
+    flush();
+    expect(test).toBeTruthy();
+  }));
 });
